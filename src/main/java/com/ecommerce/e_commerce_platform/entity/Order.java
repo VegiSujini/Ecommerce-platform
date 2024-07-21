ccc5f38a8b1fd9ca494aee57b267ef08db9ffd69
VegiSujini <sujini.vegi@gmail.com>
2024-07-21 12:41:23 +0530

    added order entity

---
+++ src/main/java/com/ecommerce/e_commerce_platform/entity/Order.java
@@ -0,0 +1,70 @@
+package com.ecommerce.e_commerce_platform.entity;
+
+import java.sql.Date;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+
+import org.springframework.data.annotation.CreatedDate;
+
+@Entity
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long orderId;
+    @CreatedDate
+    private Date orderDate;
+    private double totalAmount;
+    private boolean status;
+
+    @OneToMany(cascade = CascadeType.PERSIST)
+    @JoinColumn
+    private List<Customer> customers;
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public Date getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(Date orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public double getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(double totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public List<Customer> getCustomers() {
+        return customers;
+    }
+
+    public void setCustomers(List<Customer> customers) {
+        this.customers = customers;
+    }
+
+}
