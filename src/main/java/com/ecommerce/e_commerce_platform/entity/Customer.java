5ad12d6c653690af9089bfe21d11d273ad4cce23
VegiSujini <sujini.vegi@gmail.com>
2024-07-21 12:36:29 +0530

    added customer file

---
+++ src/main/java/com/ecommerce/e_commerce_platform/entity/Customer.java
@@ -0,0 +1,50 @@
+package com.ecommerce.e_commerce_platform.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long customerId;
+
+    private String name;
+    private String email;
+    private String address;
+
+    public Long getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Long customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+}
