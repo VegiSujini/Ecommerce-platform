4d72ebfc912da921df36d16cdb7b899fbc07167a
VegiSujini <sujini.vegi@gmail.com>
2024-07-21 12:31:26 +0530

    some changes

---
+++ src/main/java/com/ecommerce/e_commerce_platform/entity/Product.java
@@ -0,0 +1,59 @@
+package com.ecommerce.e_commerce_platform.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long productId;
+
+    private String name;
+    private String description;
+    private double price;
+    private int stockQuantity;
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getStockQuantity() {
+        return stockQuantity;
+    }
+
+    public void setStockQuantity(int stockQuantity) {
+        this.stockQuantity = stockQuantity;
+    }
+
+}
